dab931bafd078b22464e33c227218f7b4d68e79a
Abhilash Hari <abhilashhari1313@gmail.com>
2023-09-09 09:27:54 +0530

    Updated

--- Wisher/urls.py
+++ Wisher/urls.py
@@ -15,8 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf import settings
-from django.contrib.staticfiles.urls import static 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns 
 from . import views
 
